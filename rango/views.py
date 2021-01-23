e74c96559bd6be3a3bdfe02e6b12a9eb79c0e705
Silas Lawrence <2483920l@student.gla.ac.uk>
2021-01-23 14:27:12 +0000

    chapter 3

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    # Multiline string with quotes
+    return HttpResponse("Rango says hey there partner!"
+     "<a href='/rango/about/'>Learn more<a/>")
+
+def about(request):
+    # Multiline string with backslash (less common?)
+    return HttpResponse("What is my purpose in life?\
+     <a href='/rango/'>Go back</a>")
