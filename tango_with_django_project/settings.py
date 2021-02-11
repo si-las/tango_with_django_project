aeb06981980d0b1977fd856b6e6dc0eacc343124
Silas Lawrence <2483920l@student.gla.ac.uk>
2021-02-11 18:28:46 +0000

    Chapter 10

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = 'rango:login'
 
 # Application definition
 
@@ -96,6 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
